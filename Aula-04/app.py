df8a35cc8a794152379260419c9704b767ebf399
Junior Fernandes <crfjunior65@gmail.com>
2025-08-08 14:45:55 -0300

    Atividades da Aula 4

--- Aula-04/app.py
+++ Aula-04/app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # --- Carregamento dos dados ---
-df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/crfjunior65/idcPython-alura/blob/A7X3D9/Aula-04/dados-imersao-final-Dia04.csv")
 
 # --- Barra Lateral (Filtros) ---
 st.sidebar.header("🔍 Filtros")
